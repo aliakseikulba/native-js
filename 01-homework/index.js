e197bd0eddbfae3fa0c76dae81d252ea6ec1ee88
aliakseikulba <kulba.av@gmail.com>
2021-10-19 14:51:16 +0300

    task 5

--- 01-homework/index.js
+++ 01-homework/index.js
@@ -21,3 +21,10 @@
 // }
 // console.log(arr);
 
+// 5. Заполните массив числами от 1 до 10 с помощью цикла
+// const arr = [];
+// for (let i = 1; i < 11; i++) {
+//   arr.push(i)
+// }
+// console.log(arr);
+
