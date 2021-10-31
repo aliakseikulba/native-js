208e4951cde742c37e1af44b0bcb4e488b7e921a
aliakseikulba <kulba.av@gmail.com>
2021-10-31 21:07:17 +0300

    all buttons events

---
+++ 03-events/index.js
@@ -0,0 +1,22 @@
+// происходит событие
+// генерируется объект со сведениями о произошедшем событии
+// вызывается функция-обработчик
+// объект со сведениями передается в качестве параметра в обработчик
+
+const sml = document.getElementById("small");
+const mdm = document.getElementById("medium");
+const big = document.getElementById("big");
+
+const onClickHandler = (e) => {
+  if (e.target.tagName === 'BUTTON') { //делегирование обработки события на все дочерние кнопки
+    alert(e.target.name);
+  }
+};
+
+sml.addEventListener('click', onClickHandler);
+// mdm.addEventListener('click', onClickHandler);
+// big.addEventListener('click', onClickHandler);
+
+
+//e.target - элемент, который сгенерировал событие
+//e.currentTarget - элемент, который вызвал событие в процессе всплытия
