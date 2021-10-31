f50aee52cf8f1a76dcc630f7e404eacc41fc962b
aliakseikulba <kulba.av@gmail.com>
2021-10-31 22:39:43 +0300

    stopPropagation

--- 03-events/index.js
+++ 03-events/index.js
@@ -7,15 +7,21 @@ const sml = document.getElementById("small");
 const mdm = document.getElementById("medium");
 const big = document.getElementById("big");
 
+// const onClickHandler = (e) => {
+//   if (e.target.tagName === 'BUTTON') { //делегирование обработки события на все дочерние кнопки
+//     alert(e.target.name);
+//   }
+// };
+
+
 const onClickHandler = (e) => {
-  if (e.target.tagName === 'BUTTON') { //делегирование обработки события на все дочерние кнопки
-    alert(e.target.name);
-  }
+  e.stopPropagation(); // прекратить всплытие
+  console.log(e.currentTarget.id);
 };
 
 sml.addEventListener('click', onClickHandler);
-// mdm.addEventListener('click', onClickHandler);
-// big.addEventListener('click', onClickHandler);
+mdm.addEventListener('click', onClickHandler);
+big.addEventListener('click', onClickHandler);
 
 
 //e.target - элемент, который сгенерировал событие
