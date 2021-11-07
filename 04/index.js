50fd871ab1f3bc3c1d5b37c6d10c428e64003a45
aliakseikulba <kulba.av@gmail.com>
2021-11-07 19:38:47 +0300

    add variables to id

--- 04/index.js
+++ 04/index.js
@@ -1,22 +1,28 @@
-const todoList = [
+const todoListsId_1 = '23ir-67nb'
+const todoListsId_2 = '24ir-88nb'
+
+const todoLists = [
   {
-    id: '23ir-67nb',
+    id: todoListsId_1,
     title: 'What to learn',
-    filter: 'all',
-    tasks: [
-      {id: 'qws2s3', title: 'JS', isDone: true},
-      {id: 'qgh2s1', title: 'CSS', isDone: true},
-      {id: 'qws343', title: 'React', isDone: true},
-    ]
+    filter: 'all'
   },
   {
-    id: '24ir-88nb',
+    id: todoListsId_2,
     title: 'What to buy',
-    filter: 'all',
-    tasks: [
-      {id: 'avb31f', title: 'Meat', isDone: true},
-      {id: 'am451f', title: 'Bread', isDone: false},
-      {id: 'ash56k', title: 'Milk', isDone: true},
-    ]
+    filter: 'all'
   },
 ];
+
+const tasks = {
+  [todoListsId_1]: [
+    {id: 'qws2s3', title: 'JS', isDone: true},
+    {id: 'qgh2s1', title: 'CSS', isDone: true},
+    {id: 'qws343', title: 'React', isDone: true},
+  ],
+  [todoListsId_2]: [
+    {id: 'avb31f', title: 'Meat', isDone: true},
+    {id: 'am451f', title: 'Bread', isDone: false},
+    {id: 'ash56k', title: 'Milk', isDone: true},
+  ]
+}
