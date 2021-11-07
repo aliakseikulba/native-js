192524200ce60b236ee549804fd749053680b7bb
aliakseikulba <kulba.av@gmail.com>
2021-11-07 19:24:43 +0300

    tasks array

---
+++ 04/index.js
@@ -0,0 +1,22 @@
+const todoList = [
+  {
+    id: '23ir-67nb',
+    title: 'What to learn',
+    filter: 'all',
+    tasks: [
+      {id: 'qws2s3', title: 'JS', isDone: true},
+      {id: 'qgh2s1', title: 'CSS', isDone: true},
+      {id: 'qws343', title: 'React', isDone: true},
+    ]
+  },
+  {
+    id: '24ir-88nb',
+    title: 'What to buy',
+    filter: 'all',
+    tasks: [
+      {id: 'avb31f', title: 'Meat', isDone: true},
+      {id: 'am451f', title: 'Bread', isDone: false},
+      {id: 'ash56k', title: 'Milk', isDone: true},
+    ]
+  },
+];
