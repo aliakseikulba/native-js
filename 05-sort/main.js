e6b01b137c69ef06466262141adb83f26ac975f7
aliakseikulba <kulba.av@gmail.com>
2021-11-16 11:29:41 +0300

    bubble sort refactored

--- 05-sort/main.js
+++ 05-sort/main.js
@@ -1,10 +1,9 @@
-const arr = [1, 23, 45, 642, 678,12, 34]
+const arr = [1, 23, 0, 45, 642, 678,12, 34]
 
 
 //bubble sort
-
-for (let j = 0; j < arr.length; j++) {
-  for (let i = 0; i < arr.length; i++) {
+for (let j = 0; j < arr.length - 1; j++) {
+  for (let i = 0; i < arr.length - 1; i++) {
     if (arr[i] > arr[i +1]) {
       let temp = arr[i + 1];
       arr[i + 1] = arr[i];
@@ -12,3 +11,4 @@ for (let j = 0; j < arr.length; j++) {
     }
   }
 }
+console.log(arr);
