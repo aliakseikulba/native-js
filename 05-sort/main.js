57a4509c3e969a5d55ba3ca839b32b8e8100766f
aliakseikulba <kulba.av@gmail.com>
2021-11-16 11:34:50 +0300

    bubble sort optimized

--- 05-sort/main.js
+++ 05-sort/main.js
@@ -3,7 +3,7 @@ const arr = [1, 23, 0, 45, 642, 678,12, 34]
 
 //bubble sort
 for (let j = 0; j < arr.length - 1; j++) {
-  for (let i = 0; i < arr.length - 1; i++) {
+  for (let i = 0; i < arr.length - 1 - j; i++) {
     if (arr[i] > arr[i +1]) {
       let temp = arr[i + 1];
       arr[i + 1] = arr[i];
