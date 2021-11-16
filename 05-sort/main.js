b6225ff5100a55861bbc4f20d29e92d7f4217604
aliakseikulba <kulba.av@gmail.com>
2021-11-16 11:26:01 +0300

    bubble sort

--- 05-sort/main.js
+++ 05-sort/main.js
@@ -1,1 +1,14 @@
 const arr = [1, 23, 45, 642, 678,12, 34]
+
+
+//bubble sort
+
+for (let j = 0; j < arr.length; j++) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > arr[i +1]) {
+      let temp = arr[i + 1];
+      arr[i + 1] = arr[i];
+      arr[i] = temp
+    }
+  }
+}
