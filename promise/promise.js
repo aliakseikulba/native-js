c8f3dabfe2c201766577a72b2b973b61ebba02ce
aliakseikulba <kulba.av@gmail.com>
2022-01-07 17:11:10 +0300

    promise part 1 theory

--- promise/promise.js
+++ promise/promise.js
@@ -1,11 +1,17 @@
 let axios = {
   get() {
-    return new Promise( (res, rej) => {
+    return new Promise((res, rej) => {
       setTimeout(() => {
-        res('hello')
-      }, 3000)
-    })
+        res({
+          a: 'adsas',
+          b: 'sdaa',
+          message: 'hello',
+        });
+      }, 3000);
+    });
   }
-}
+};
 
-axios.get().then(res => console.log(res));
+axios.get().then(res => res.message)
+.then(data => console.log(data)) //hello
+.then(res => console.log(res)) //undefined
